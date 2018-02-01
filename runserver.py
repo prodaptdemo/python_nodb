2af33ff2314030a8cbe1cf3df406017d2ecb86d1
prodapt <mahendran.m@prodapt.com>
2018-02-01 15:01:22 +0530

    hello

---
+++ runserver.py
@@ -0,0 +1,9 @@
+"""
+This script runs the FlaskWebProject1 application using a development server.
+"""
+
+from os import environ
+from App import app
+
+if __name__ == '__main__':       
+    app.run(host='0.0.0.0',port='5555')
